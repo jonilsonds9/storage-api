65aa66204ec9ce2c5696ef5b4d425cdffeb24e60
Jonilson Sousa <jonilsonds9@gmail.com>
2022-04-26 16:05:41 -0300

    Usando o skywalking agent

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import agent from 'skywalking-backend-js';
 
 async function bootstrap() {
+  agent.start({
+    serviceName: 'storage-api-service',
+    serviceInstance: 'storage-api-instance',
+    collectorAddress: '127.0.0.1:11800',
+  });
+
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.useGlobalPipes(new ValidationPipe());
 
