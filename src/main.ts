fe6c66df122d0d938c28f7fc1520e88260f37d25
Jonilson Sousa <jonilsonds9@gmail.com>
2022-04-27 21:18:40 -0300

    Variavel de ambiente tracing

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import agent from 'skywalking-backend-js';
 
 async function bootstrap() {
-  agent.start({
-    serviceName: 'storage-api-service',
-    serviceInstance: 'storage-api-instance',
-    collectorAddress: '127.0.0.1:11800',
-  });
+  
+  if (process.env.ENABLE_TRACING === 'true') {
+    agent.start({
+      serviceName: 'storage-api-service',
+      serviceInstance: 'storage-api-instance',
+      collectorAddress: '127.0.0.1:11800',
+    });
+  }
 
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.useGlobalPipes(new ValidationPipe());
