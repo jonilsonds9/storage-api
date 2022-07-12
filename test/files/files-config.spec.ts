d50bf9873dc3aa0293d62b5aa1ec11d3ff9bc2a5
Jonilson Sousa <jonilsonds9@gmail.com>
2022-07-12 11:18:10 -0300

    Removendo teste desnecessario

--- test/files/files-config.spec.ts
+++
@@ -1,7 +0,0 @@
-import { StorageConfig } from '../../src/config/storage';
-
-describe('FilesConfig', () => {
-  it('should be defined', () => {
-    expect(new StorageConfig()).toBeDefined();
-  });
-});
